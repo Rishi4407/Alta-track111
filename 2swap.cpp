8b346407def46a6ed503c273f3f43d900520cafd
Rishi4407 <agrawalrishi4407@gmail.com>
2026-09-17 23:20:52 +0530

    Add files via upload

---
+++ 2swap.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10, b=5;
+    int c;
+    c=a;
+    a=b;
+    b=c;
+    cout<< "Value of a is:"<<a<<endl;
+    cout<< "Value of b is:"<<b<<endl;
+}
