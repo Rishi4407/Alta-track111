39224c5eeb9857a458dcfa2cf62295ea9867db2a
Rishi4407 <agrawalrishi4407@gmail.com>
2026-09-24 10:36:17 +0530

    Add files via upload

---
+++ 8Season.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the no. of Month:";
+    cin>>n;
+
+    switch (n)
+    {
+        case 1:
+        cout<<"Season:Winter"<<endl;
+        break;
+    
+        case 2:
+        cout<<"Season:Winter"<<endl;
+        break;
+
+        case 3:
+        cout<<"Season:Spring"<<endl;
+        break;
+
+        case 4:
+        cout<<"Season:Spring"<<endl;
+        break;
+
+        case 5:
+        cout<<"Season:Spring"<<endl;
+        break;
+
+        case 6:
+        cout<<"Season:Summer"<<endl;
+        break;
+
+        case 7:
+        cout<<"Season:Summer"<<endl;
+        break;
+
+        case 8:
+        cout<<"Season:Summer"<<endl;
+        break;
+
+        case 9:
+        cout<<"Season:Autumn/Monsoon"<<endl;
+        break;
+
+        case 10:
+        cout<<"Season:Autumn/Monsoon"<<endl;
+        break;
+
+        case 11:
+        cout<<"Season:Autumn/Monsoon"<<endl;
+        break;
+
+        case 12:
+        cout<<"Season:Winter"<<endl;
+        break;
+
+        default:
+        cout<<"Enter a valid Month";
+        break;
+    }
+
+}
